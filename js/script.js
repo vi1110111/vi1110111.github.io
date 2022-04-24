ddb66dce2515fc45eb566c493dd0bafc9c0ca6d2
vi1110111 <88076376+vi1110111@users.noreply.github.com>
2022-04-24 23:05:50 +0300

    Second commit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 const menu = document.querySelector(".menu");
 const header = document.querySelector(".header");
+const footer = document.querySelector(".footer");
 
 document
   .querySelector(".header__burger")
@@ -10,3 +11,30 @@ document
   });
 
 console.log("changed");
+
+let lastScroll = 0;
+const defaultOffset = 200;
+
+const scrollPosition = () =>
+  window.pageYOffset || document.documentElement.scrollTop;
+const containsHide = () => header.classList.contains("header-hide");
+
+window.addEventListener("scroll", function () {
+  if (
+    scrollPosition() > lastScroll &&
+    !containsHide() &&
+    scrollPosition() > defaultOffset
+  ) {
+    header.classList.add("header-hide");
+    menu.classList.add("menu-hide");
+    footer.classList.add("footer-hide");
+    // scroll down
+  } else if (scrollPosition() < lastScroll && containsHide()) {
+    // scroll up
+
+    header.classList.remove("header-hide");
+    menu.classList.remove("menu-hide");
+    footer.classList.remove("footer-hide");
+  }
+  lastScroll = scrollPosition();
+});
